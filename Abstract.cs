e8755d80bd7479d01376ef599467510cb34b714d
Zypsie-dev <9860224200rs@gmail.com>
2024-03-12 22:46:31 +0545

    init

---
+++ Abstract.cs
@@ -0,0 +1,25 @@
+﻿abstract class Animal
+{
+    public abstract void animalSound();
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: how how");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.animalSound();
+        dog.sleep(); 
+    }
+}
---
+++ Inheritance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Dot_net
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking.");
+        }
+    }
+
+    class Mammal : Animal
+    {
+        public void givebirth()
+        {
+            Console.WriteLine("Mammal is giving birth.");
+        }
+    }
+
+    class Horse : Mammal
+    {
+        public void swim()
+        {
+            Console.WriteLine("Horse is swimming.");
+        }
+    }
+
+    class Program
+    {
+        static void seal(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Mammal mammal = new Mammal();
+            mammal.Eat();
+            mammal.givebirth();
+
+            Horse horse = new Horse();
+            horse.givebirth();
+            horse.swim();
+
+        }
+    }
+}
